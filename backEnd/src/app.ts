d4bfecf3648e973169ef41fc063857cb5844228a
daniel-dantas <daniel.dantas.ip19@hotmail.com>
2020-12-08 22:45:34 -0300

    feat: Configuring routes

--- backEnd/src/app.ts
+++ backEnd/src/app.ts
@@ -1,6 +1,9 @@
-import express, { Express } from 'express';
+import express, { Express, json, Request, Response } from 'express';
+import Cors from 'cors';
 import postgres from './databases/postgres';
 import Mongo from './databases/mongo';
+import Routes from './routes';
+
 interface AppConfig {
     PORT?: number
 }
@@ -11,7 +14,9 @@ class App {
 
     constructor({ PORT } : AppConfig){
         this.main = express();
+        this.config();
         this.connectDatabase();
+        this.routes();
         this.listen((PORT) ? PORT : 8000);
     }
 
@@ -36,6 +41,20 @@ class App {
             console.log('Connection error: ' + err);
         })
 
+    }
+
+    private routes(){
+        this.main.get('/api/v1', (req: Request, res: Response) => {
+            return res.status(200).json({
+
+            });
+        });
+        this.main.use('/api/v1', Routes);
+    }
+
+    private config(){
+        this.main.use(json());
+        this.main.use(Cors());
     }
 
 }
