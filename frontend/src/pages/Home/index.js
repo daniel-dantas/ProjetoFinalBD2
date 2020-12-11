9acfffcf52617313fbcff4f46763220f955cb6e5
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2020-12-11 07:55:03 -0300

    view: construindo listagem dos discentes

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,12 +1,150 @@
 import React from 'react';
 
-import {Main} from '../../styles/global';
+
+import {TopBar,HomeContent,StudentCard,ContentFilters, ContentCards} from './style';
+
+import userAvatar from '../../assets/img/perfil-avatar.jpg';
+import github from '../../assets/icon/github-icon.png';
+import linkedin from '../../assets/icon/linkedin-icon.png';
 
 function Home() {
+
+    const technologies = ['React','React Native','Node'];
+
     return(
-        <Main>
-            <h1>Home</h1>
-        </Main>
+        <HomeContent>
+            <TopBar>
+                <h2>Contrata meu aluno</h2>
+            </TopBar>
+            <ContentFilters>
+                <h3>Filtros</h3>
+                <select>
+                    <option>Tecnologias</option>
+                    <option>React</option>
+                    <option>Vue</option>
+                    <option>Angular</option>
+                </select>
+                <select>
+                    <option>Área de Atuação</option>
+                </select>
+            </ContentFilters>
+            <ContentCards>
+            <StudentCard>
+                    <img src={userAvatar} alt="User avatar" className="userAvatar"/>
+                    <h3>Jorge Sobrinho Neto</h3>
+                    <p>jorgeneto@email.com</p>
+                        <div className="social-links">
+                            <a>
+                                <img src={github} alt="Github link" />
+                            </a>
+                            <a>
+                                <img src={linkedin} alt="Linkedin link" />
+                            </a>
+                        </div>
+                    <ul>
+                            {technologies.map((tech,index) => (
+                                <li key={index}>{tech}</li>
+                            ))}
+                    </ul>
+                    <button>Mais</button> 
+            </StudentCard>
+            <StudentCard>
+                    <img src={userAvatar} alt="User avatar" className="userAvatar"/>
+                    <h3>Jorge Sobrinho Neto</h3>
+                    <p>jorgeneto@email.com</p>
+                        <div className="social-links">
+                            <a>
+                                <img src={github} alt="Github link" />
+                            </a>
+                            <a>
+                                <img src={linkedin} alt="Linkedin link" />
+                            </a>
+                        </div>
+                    <ul>
+                            {technologies.map((tech,index) => (
+                                <li key={index}>{tech}</li>
+                            ))}
+                    </ul>
+                    <button>Mais</button> 
+            </StudentCard>
+            <StudentCard>
+                    <img src={userAvatar} alt="User avatar" className="userAvatar"/>
+                    <h3>Jorge Sobrinho Neto</h3>
+                    <p>jorgeneto@email.com</p>
+                        <div className="social-links">
+                            <a>
+                                <img src={github} alt="Github link" />
+                            </a>
+                            <a>
+                                <img src={linkedin} alt="Linkedin link" />
+                            </a>
+                        </div>
+                    <ul>
+                            {technologies.map((tech,index) => (
+                                <li key={index}>{tech}</li>
+                            ))}
+                    </ul>
+                    <button>Mais</button> 
+            </StudentCard>
+            <StudentCard>
+                    <img src={userAvatar} alt="User avatar" className="userAvatar"/>
+                    <h3>Jorge Sobrinho Neto</h3>
+                    <p>jorgeneto@email.com</p>
+                        <div className="social-links">
+                            <a>
+                                <img src={github} alt="Github link" />
+                            </a>
+                            <a>
+                                <img src={linkedin} alt="Linkedin link" />
+                            </a>
+                        </div>
+                    <ul>
+                            {technologies.map((tech,index) => (
+                                <li key={index}>{tech}</li>
+                            ))}
+                    </ul>
+                    <button>Mais</button> 
+            </StudentCard>
+            <StudentCard>
+                    <img src={userAvatar} alt="User avatar" className="userAvatar"/>
+                    <h3>Jorge Sobrinho Neto</h3>
+                    <p>jorgeneto@email.com</p>
+                        <div className="social-links">
+                            <a>
+                                <img src={github} alt="Github link" />
+                            </a>
+                            <a>
+                                <img src={linkedin} alt="Linkedin link" />
+                            </a>
+                        </div>
+                    <ul>
+                            {technologies.map((tech,index) => (
+                                <li key={index}>{tech}</li>
+                            ))}
+                    </ul>
+                    <button>Mais</button> 
+            </StudentCard>
+            <StudentCard>
+                    <img src={userAvatar} alt="User avatar" className="userAvatar"/>
+                    <h3>Jorge Sobrinho Neto</h3>
+                    <p>jorgeneto@email.com</p>
+                        <div className="social-links">
+                            <a>
+                                <img src={github} alt="Github link" />
+                            </a>
+                            <a>
+                                <img src={linkedin} alt="Linkedin link" />
+                            </a>
+                        </div>
+                    <ul>
+                            {technologies.map((tech,index) => (
+                                <li key={index}>{tech}</li>
+                            ))}
+                    </ul>
+                    <button>Mais</button> 
+            </StudentCard>
+            </ContentCards>
+        </HomeContent>
     );
 }
 
---
+++ frontend/src/pages/Home/style.js
@@ -0,0 +1,134 @@
+import styled from 'styled-components';
+import {Main} from '../../styles/global';
+
+import Colors from '../../styles/colors';
+
+export const TopBar = styled.div`
+    width: 100%;
+    height: 70px;
+    position: fixed;
+    top: 0;
+    background-color: ${Colors.primaryColor};
+    display: flex;
+    align-items: center;
+    z-index: 100;
+
+    h2{
+        color: #FFF;
+        margin-left: 25px;
+    }
+`;
+
+export const HomeContent = styled(Main)`
+    width: 100%;
+    height: auto;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-items: center;
+    margin-top: 70px;
+`
+export const ContentFilters = styled.div`
+    width: 96%;
+    max-width: 1024px;
+    height: 70px;
+    margin-top: 15px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin-bottom: 20px;
+
+    select{
+        width: 180px;
+        height: 40px;
+        border-radius: 4px;
+        color: ${Colors.primaryColor};
+        font-size: 1em;
+        font-weight: 500;
+        border: solid 2px ${Colors.secondaryColor};
+        outline: none;
+        background-color: #FFF;
+        margin-left: 20px;
+        padding: 0px 5px;
+        position: relative;
+        margin-right: 20px;
+    }
+`
+
+export const ContentCards = styled.div`
+    width: 1024px;
+    height: auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    flex-wrap: wrap;
+`
+
+export const StudentCard = styled.div`
+    width: 28%;
+    height: auto;
+    min-height: 300px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    background-color: ${Colors.tertiaryColor};
+    box-shadow: inset 2px 2px 8px rgba(209,205,199,0.5), 6px 6px 16px rgba(255,255,255,0.5), -6px -6px 16px rgba(255,255,255,0.5);
+    margin-bottom: 30px;
+
+    .userAvatar{
+        max-width: 85px;
+        border-radius: 50px;
+        margin-bottom: 20px;
+    }
+
+    h3{
+        margin-bottom: 10px;
+    }
+
+    p{
+        font-size: 0.9em;
+        margin-bottom: 10px;
+    }
+    
+    .social-links{
+        margin-bottom: 10px;
+
+        a{
+            img{
+                max-width: 23px;
+                margin-right: 10px;
+            }
+        }
+    }
+
+    ul{
+        list-style: none;
+        margin-bottom: 10px;
+        li{
+            float: left;
+            margin-left: 5px;
+            color: ${Colors.fontColor};
+            font-weight: 500;
+
+            &::after{
+                content: " |"
+            }
+        }
+    }
+
+    button{
+        width: 60px;
+        height: 30px;
+        border: none;
+        outline: none;
+        border-radius: 6px;
+        color: #FFF;
+        font-weight: bold;
+        background-color: ${Colors.primaryColor};
+    }
+`
