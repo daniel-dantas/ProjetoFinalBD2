d350c405ec8e6d52d09b62278dc1a32aae9960d0
daniel-dantas <daniel.dantas.ip19@hotmail.com>
2020-12-10 23:25:15 -0300

    feature/PDB-12-Criar-os-middlewares-para-as-rotas

--- backEnd/src/routes/contractor.ts
+++ backEnd/src/routes/contractor.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import ContractorController from '../controllers/contractor';
+import AuthMiddleware from '../middlewares/auth';
 
 const route = Router();
 
 route.post('/', ContractorController.create);
+
+route.use(AuthMiddleware);
 route.get('/', ContractorController.read);
 route.put('/', ContractorController.update);
 route.delete('/', ContractorController.delete);
