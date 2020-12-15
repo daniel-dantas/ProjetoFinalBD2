4eaa4d1e93a23cef1706c644a02d5e18a046f2c3
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2020-12-15 09:12:38 -0300

    view: tela de listagem dos times

---
+++ frontend/src/pages/ListTeam/style.js
@@ -0,0 +1,115 @@
+import styled from 'styled-components';
+import {Main} from '../../styles/global';
+
+import Colors from '../../styles/colors';
+
+export const TeamContent = styled(Main)`
+    width: 100%;
+    height: auto;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-items: center;
+    margin-top: 70px;
+`
+export const ContentCards = styled.div`
+    width: 1024px;
+    height: auto;
+    min-height: 50vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    position: relative;
+    align-items: center;
+`
+
+export const StudentCard = styled.div`
+    width: 28%;
+    height: auto;
+    min-height: 300px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    background-color: ${Colors.tertiaryColor};
+    box-shadow: inset 2px 2px 8px rgba(209,205,199,0.5), 6px 6px 16px rgba(255,255,255,0.5), -6px -6px 16px rgba(255,255,255,0.5);
+    margin-bottom: 50px;
+
+    .userAvatar{
+        max-width: 85px;
+        border-radius: 50px;
+        margin-bottom: 20px;
+    }
+
+    h3{
+        margin-bottom: 10px;
+    }
+
+    p{
+        font-size: 0.9em;
+        margin-bottom: 10px;
+    }
+    
+    .social-links{
+        margin-bottom: 10px;
+
+        a{
+            img{
+                max-width: 23px;
+                margin-right: 10px;
+            }
+        }
+    }
+
+    ul{
+        list-style: none;
+        margin-bottom: 10px;
+        li{
+            float: left;
+            margin-left: 5px;
+            color: ${Colors.fontColor};
+            font-weight: 500;
+
+            &::after{
+                content: " |"
+            }
+        }
+    }
+
+    button{
+        width: 70px;
+        height: 30px;
+        border: none;
+        outline: none;
+        border-radius: 6px;
+        color: #FFF;
+        font-weight: bold;
+        background-color: ${Colors.primaryColor};
+        transition: all 0.2s;
+        cursor: pointer;
+
+        &:hover{
+            opacity: 0.9;
+        }
+    }
+`;
+
+export const CloseTeam = styled.button`
+    width: 150px;
+    height: 50px;
+    outline: none;
+    border: none;
+    background-color: ${Colors.primaryColor};
+    position: fixed;
+    bottom: 30px;
+    right: 30px;
+    cursor: pointer;
+    color: #FFF;
+    font-weight: bold;
+    border-radius: 8px;
+`;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -10,17 +10,19 @@ import RegisterStudent from './pages/RegisterStudent';
 import Profile from './pages/Profile';
 import RegisterProject from './pages/RegisterProject';
 import ListProjects from './pages/ListProjects';
+import ListTeam from './pages/ListTeam';
 
 function Routes(){
     return(
         <BrowserRouter>
-             <PrivateRouter path="/" exact component={Home} />
+            <PrivateRouter path="/" exact component={Home} />
             <Route path="/login" exact component={Login} />
             <Route path="/company" exact component={RegisterCompany} />
             <Route path="/student" exact component={RegisterStudent}/>
             <Route path="/profile" component={Profile} /> 
             <Route path="/project" component={RegisterProject}/>
             <Route patch="/projects" component={ListProjects} />
+            <Route patch="/team" component={ListTeam} />
         </BrowserRouter>
     );
 }
